919268aeb1e52742ec7a912e75008c48c68d1564
whdev36 <wisdomhunter36@gmail.com>
2024-11-25 11:23:23 -0800

    test api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
@@ -11,7 +13,7 @@ class HelloWorld(Resource):
     def post(self):
         return {'message': 'Posted successfully!'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/hello')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 
 BASE_URL = 'http://127.0.0.1:5000/'
 
-# resp = requests.get(BASE_URL)
-resp = requests.post(BASE_URL)
+resp = requests.get(BASE_URL + 'hello')
+# resp = requests.post(BASE_URL + 'hello')
 
 # print(resp.status_code)
 print(resp.json())
