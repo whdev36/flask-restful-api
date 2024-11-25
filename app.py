972b8ffc495562ac9645048e7e80fba3b6e7a8ee
whdev36 <wisdomhunter36@gmail.com>
2024-11-25 11:57:31 -0800

    basic resources

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_cors import CORS
+import resources as r # "r" is "resources"
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello World!'}
-    
-    def post(self):
-        return {'message': 'Posted successfully!'}
-
-api.add_resource(HelloWorld, '/hello')
+api.add_resource(r.HelloWorld, '/hw') # "hw" is "helloworld"
+api.add_resource(r.Hello, '/hello/<string:name>') # This is Hello resource
+api.add_resource(r.Student, '/student/<string:name>/<string:surname>/<int:age>/<int:id>') # This is Student resource
+api.add_resource(r.Persons, '/persons') # Persons resource
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import requests
 
 BASE_URL = 'http://127.0.0.1:5000/'
 
-resp = requests.get(BASE_URL + 'hello')
-# resp = requests.post(BASE_URL + 'hello')
+# resp = requests.get(BASE_URL + 'hw')
+# resp = requests.get(BASE_URL + 'hello/john')
+# resp = requests.get(BASE_URL + 'student/john/doe/19/12312')
+resp = requests.get(BASE_URL + 'persons')
 
 # print(resp.status_code)
 print(resp.json())
